d75f2405cd23a8ceb43c384d54e0566356c5a5b0
Danilovea1313 <137881598+Danilovea1313@users.noreply.github.com>
2023-06-28 11:00:10 +0300

    Update counter.js

--- counter.js
+++ counter.js
@@ -34,18 +34,29 @@ var counterStore = new CounterStore();
 // Добавление кнопок инкремента/декремента
 var incrementButton = document.createElement('button');
 incrementButton.textContent = '+';
+incrementButton.style.fontSize='24px';
+incrementButton.style.margin='0 10px';
+incrementButton.style.padding='5px 10px';
 incrementButton.addEventListener('click', function () {
     counterStore.incrementCounter();
     updateCounterValue();
 });
 var decrementButton = document.createElement('button');
 decrementButton.textContent = '-';
+decrementButton.style.fontSize='24px';
+decrementButton.style.margin='0 10px';
+decrementButton.style.padding='5px 10px';
 decrementButton.addEventListener('click', function () {
     counterStore.decrementCounter();
     updateCounterValue();
 });
 // Вывод состояния счетчика на экран
 var counterValueElement = document.createElement('div');
+counterValueElement.style.fontSize='24px';
+counterValueElement.style.margin='0 42px';
+counterValueElement.style.padding='0px 10px';
+counterValueElement.style.position='absolute';
+counterValueElement.style.top='15px';
 counterValueElement.textContent = counterStore.getCounterValue().toString();
 function updateCounterValue() {
     counterValueElement.textContent = counterStore.getCounterValue().toString();
